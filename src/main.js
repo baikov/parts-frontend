e6c65767cc865d0f4be49aad59225e538e7b71e9
Alex Baikov <baikov52@gmail.com>
2020-12-10 21:58:13 +0300

    add axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+// export const HTTP = axios.create({
+//   baseURL: 'http://127.0.0.1:8000/api/parts/'
+// })
 
 Vue.config.productionTip = false
 
